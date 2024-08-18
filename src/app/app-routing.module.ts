5f45168aa1acf7fde45abfd8c416327a7b2cb538
Pointifix <35230128+Pointifix@users.noreply.github.com>
2024-08-18 18:19:46 +0200

    add rounting, replace default landing page with simple navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactAddComponent } from "./contact-add/contact-add.component";
+import { ContactListComponent } from "./contact-list/contact-list.component";
+import { ContactDetailsComponent } from "./contact-details/contact-details.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: ContactAddComponent },
+  { path: 'list', component: ContactListComponent },
+  { path: 'details', component: ContactDetailsComponent },
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  /*{ path: '**', component: PageNotFoundComponent },*/
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

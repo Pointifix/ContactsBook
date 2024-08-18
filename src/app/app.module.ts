7f0027718429a900e7b6f220b49557904f132e05
Pointifix <35230128+Pointifix@users.noreply.github.com>
2024-08-18 17:36:13 +0200

    add components and contact model

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactAddComponent } from './contact-add/contact-add.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactDetailsComponent } from './contact-details/contact-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactAddComponent,
+    ContactListComponent,
+    ContactDetailsComponent
   ],
   imports: [
     BrowserModule,
